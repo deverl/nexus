69654919dd693ac634bd3f805f60a835dec4627a
DeVerl Stokes <deverl.stokes@warrantyhub.com>
2026-05-21 13:52:07 -0600

    Modified to work with new file names.

--- bin/warhub/cp_to_bb.sh
+++ bin/warhub/cp_to_bb.sh
@@ -9,5 +9,5 @@ scp $INFRA/build-box/usr/local/bin/build_and_deploy.py bb:/usr/local/bin
 
 scp $INFRA/build-box/usr/local/etc/build_and_deploy_vanguard.json bb:/usr/local/etc
 
-ssh bb 'bin/sign_json.sh'
+ssh bbn 'bin/sign_json.sh'
 
