abac5302f67a2c326333f0a1aea6467e180b1ed4
DeVerl Stokes <deverl.stokes@warrantyhub.com>
2026-07-21 13:50:56 -0600

    added some excludes.

--- bin/warhub/bkup.sync.sh
+++ bin/warhub/bkup.sync.sh
@@ -4,23 +4,14 @@ TARGET_BKUP_DIR=~/opt/db_bkup
 
 REMOTE_BASE_DIR='depot:/mnt/sfo3_dbdump_storage'
 
-EXCLUDES='--exclude _backup.log.xz --exclude defaultdb.tar.xz.age'
+EXCLUDES=(--exclude _backup.log.xz \
+          --exclude defaultdb.tar.xz.age \
+          --exclude unzip --exclude 'delme*')
 
 mkdir -p $TARGET_BKUP_DIR
 
-rsync -av $EXCLUDES $REMOTE_BASE_DIR/backups/current/      $TARGET_BKUP_DIR
+rsync -av "${EXCLUDES[@]}" $REMOTE_BASE_DIR/backups/current/      $TARGET_BKUP_DIR
 
-rsync -av $EXCLUDES $REMOTE_BASE_DIR/backups-s002/current/ $TARGET_BKUP_DIR
+rsync -av "${EXCLUDES[@]}" $REMOTE_BASE_DIR/backups-s002/current/ $TARGET_BKUP_DIR
 
-rsync -av $EXCLUDES $REMOTE_BASE_DIR/backupsdev/current/   $TARGET_BKUP_DIR
-
-#
-# eval $(op signin)
-#
-# for F in *.age
-# do
-#     echo $(basename $F .tar.xz.age)
-#     BN=$(basename $F .age)
-#     op read "op://Development/Development DB Backups AGE Key/Private Key" | age -d -i - -o $BN $F
-# done
-#
+rsync -av "${EXCLUDES[@]}" $REMOTE_BASE_DIR/backupsdev/current/   $TARGET_BKUP_DIR
